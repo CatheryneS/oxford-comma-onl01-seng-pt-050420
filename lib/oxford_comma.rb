7d63fcc98361b2825872a7c2e26fae3d6cccf496
Catheryne Small <catheryn.small@gmail.com>
2020-05-14 01:24:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return "#{array[0]} and #{array[1]}"
   end
   if array.length > 2
-    return array[-1].insert(0, "and ")
+    array[-1].insert(0, "and ")
   end
   array.join(", ")    
 end
